fd2cea09565831216e9427465f6d5c6da27800cc
Cainã Oliveira <csoliveira01@gmail.com>
2024-11-25 19:48:27 -0400

    changing some infos

--- src/main/kotlin/com/example/forum/service/TopicService.kt
+++ src/main/kotlin/com/example/forum/service/TopicService.kt
@@ -23,7 +23,7 @@ class TopicService(
     /*private val em: EntityManager*/
 ) {
 
-//    @Cacheable(cacheNames = ["topicListCache"], key = "#root.method.name")
+    @Cacheable(cacheNames = ["topicListCache"], key = "#root.method.name")
     fun list(courseName: String?, pagination: Pageable): Page<TopicView> {
         val topicList = if (courseName == null) {
             repository.findAll(pagination)
